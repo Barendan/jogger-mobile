ed0eb0b3de719a6ca2967cdaf7cb3289d373639a
Barendan <barendan@gmail.com>
2021-01-08 19:04:26 -0500

    make signup dispatch onpress and redirect

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'http://60839b66bcf9.ngrok.io',
+});
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,14 +1,48 @@
+import { AsyncStorage } from 'react-native';
 import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
+import { navigate } from '../navigationRef';
 
 const authReducer = (state, action) => {
   switch (action.type) {
+    case 'add_error':
+      return { errorMessage: '', errorMessage: action.payload };
+    case 'signup':
+      return { ...state, token: action.payload };
     default:
       return state;
   }
 };
 
+const signup = (dispatch) => async ({ email, password }) => {
+  try {
+    const response = await trackerApi.post('/signup', { email, password });
+    await AsyncStorage.setItem('token', response.data.token);
+    dispatch({
+      type: 'signup',
+      payload: response.data.token,
+    });
+
+    navigate('TrackList');
+  } catch (err) {
+    dispatch({
+      type: 'add_error',
+      payload: 'Something went wrong with sign up',
+    });
+    console.log('err', err);
+  }
+};
+
+const signin = (dispatch) => {
+  return ({ email, password }) => {};
+};
+
+const signout = (dispatch) => {
+  return () => {};
+};
+
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
-  { isSignedIn: false }
+  { signup, signin, signout },
+  { token: null, errorMessage: '' }
 );
